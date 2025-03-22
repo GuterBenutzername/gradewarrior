6dd7427d3d273afdaae75a33c1db397e24bf379f
Adam Y. Cole II <afewburritos@gmail.com>
2025-03-22 17:35:02 -0500

    front

---
+++ gw-front/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "GradeWarrior frontend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            deno
+            nodejs
+          ];
+        };
+
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "gradewarrior-frontend";
+            src = ./.;
+            
+            nativeBuildInputs = with pkgs; [
+              deno
+	      nodejs
+            ];
+
+            buildPhase = ''
+              export HOME=$(mktemp -d)
+              deno task build
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r dist/* $out/
+            '';
+          };
+        };
+      }
+    );
+}
