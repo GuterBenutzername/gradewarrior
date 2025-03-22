e884cf0f821982d2bb9bc5997ae0996e63fbf166
Adam Y. Cole II <afewburritos@gmail.com>
2025-03-22 17:25:40 -0500

    \

---
+++ gw-back/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "GradeWarrior Backend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313
+            python313Packages.ariadne
+            python313Packages.sqlalchemy
+            python313Packages.uvicorn
+            python313Packages.starlette
+	    python313Packages.psycopg2
+          ];
+        };
+      }
+    );
+}
